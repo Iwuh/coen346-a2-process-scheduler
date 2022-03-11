d9afd414d28b6b2a413aaac73d90f9176a437c96
Matthew Faigan <matthew.faigan@gmail.com>
2022-03-10 21:14:42 -0500

    Create clock

---
+++ clock.cpp
@@ -0,0 +1,42 @@
+#include "include/clock.h"
+
+#include <exception>
+#include <thread>
+
+// How many clock cycles to simulate before exiting
+constexpr auto maxCycles = 100U;
+// How many real milliseconds to wait between each simulated clock cycle
+constexpr auto cycleLength = 250U;
+
+Clock& Clock::getInstance()
+{
+    // The static instance persists between calls, so the first getInstance() call will create the instance and future calls will return the same instance.
+    static Clock instance;
+    return instance;
+}
+
+Clock::Clock()
+    : time(0), started(false)
+{
+}
+
+int Clock::getTime() const
+{
+    // Get a shared lock allowing any number of readers, as long as nobody is writing
+    std::shared_lock readerLock(mutex);
+    return time;
+}
+
+void Clock::operator()()
+{
+    if (started) throw std::runtime_error("Clock has already been started");
+
+    started = true;
+    for (unsigned int count = 0; count < maxCycles; count++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(cycleLength));
+        // Get a unique lock that blocks all readers while the increment happens
+        std::unique_lock writerLock(mutex);
+        time++;
+    }
+}
---
+++ include/clock.h
@@ -0,0 +1,27 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <shared_mutex>
+
+// Simulates the clock used by the OS when scheduling processes. Thread-safe, uses the singleton pattern to share one instance through getInstance().
+// Singleton pattern based on: https://stackoverflow.com/a/1008289
+class Clock
+{
+public:
+    static Clock& getInstance();
+
+public:
+    Clock(const Clock& other) = delete;
+    void operator=(const Clock& other) = delete;
+
+    int getTime() const;
+    void operator()();
+
+private:
+    Clock();
+    mutable std::shared_mutex mutex;
+    int time;
+    bool started;
+};
+
+#endif
